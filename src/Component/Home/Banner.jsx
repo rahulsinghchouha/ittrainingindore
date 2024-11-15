d7f24dbb443b67cfc6d7fe57cbaf22b61a0da1d0
rahulsinghchouha <singhrahul15737@gmail.com>
2024-11-15 19:13:15 +0530

    almost banner complete not full

--- src/Component/Home/Banner.jsx
+++ src/Component/Home/Banner.jsx
@@ -1,5 +1,7 @@
+import Search from "antd/es/transfer/search";
 import { Field, Formik,Form } from "formik";
 import React from "react";
+import { CiSearch } from "react-icons/ci";
 
 
 function Banner() {
@@ -11,7 +13,7 @@ function Banner() {
     return (
         <section className="home-banner-wr">
             <div className="wrapper">
-                <div className="box-border ">
+                <div className="box-border flex justify-between ">
                     <div className="w-[47.5%] pt-[16px] pl-0 pr-0 pb-0">
                         <div>
                             <h4 className="get-course-now text-[14px] leading-[20px] text-[#1aaef4] font-[400] pr-[70px] inline-block">GET COURSE NOW</h4>
@@ -33,6 +35,7 @@ function Banner() {
                                 Welcome to IT Training Indore, your premier destination for comprehensive IT training and placements in Indore.
                             </p>
                         </div>
+                        {/* banner search form */}
                         <div className="w-[100%] mt-[16px] ">
                             <Formik
                                 initialValues={{ query: "" }}
@@ -40,8 +43,11 @@ function Banner() {
 
                             >
                                 {() => (
-                                    <form>
-                                        <input type="text" name="query" placeholder="Search Course" className="border-none py-3 px-4 rounded-full" />
+                                    <form className="w-[98%] flex ">
+                                        <input type="text" name="query" placeholder="Search Course" className="bg-[#ffffff] py-[17px]  px-[30px] w-[83%] rounded-tl-[27px] rounded-r-[0px] rounded-b-[0px] rounded-l-[27px] search-shadow focus:outline-none" />
+                                        <button className="  bg-[#1aaef4] w-[17%] rounded-r-full items-center cursor-pointer "> 
+                                                   <input  className="search-submit ml-8 focus:outline-none cursor-pointer " />
+                                        </button>
 
                                     </form>
 
@@ -49,6 +55,45 @@ function Banner() {
                             </Formik>
 
                         </div>
+                        {/* popular searches course link */}
+                        <div className="mt-[25px]">
+                            <div className="inline-block">
+                                <span className="text-[16px] leading-[21px] text-[#000000] font-[600] ">Popular Searches</span>
+
+                            </div>
+                            <div className="ml-[10px] inline-block cursor-pointer">
+                                <a href="" className="py-[9px] px-[15px] textColor rounded-[18px] leading-[16px] font-[500] border-solid border-[0.4px] border-[#1aaef4] transition-all duration-300 ease ">Web Development</a>
+                                <a href="" className="py-[9px] px-[15px] ml-[5px] textColor rounded-[18px] leading-[16px] font-[500] border-solid border-[0.4px] border-[#1aaef4] transition-all duration-300 ease ">Graphic Design</a>
+                                <a href="" className="py-[9px] px-[15px] ml-[5px] textColor rounded-[18px] leading-[16px] font-[500] border-solid border-[0.4px] border-[#1aaef4] transition-all duration-300 ease ">Animation</a>
+                            </div>
+                        </div>
+
+                        {/* banner course link */}
+                            <div className="mt-[60px] box-border flex">
+                                <div className="w-[32.8%] pl-[20px] border-r-[1px] border-solid border-[#e0e0e0]">
+                                    <img  src="/Home/web_development_icon.png" alt="development-icon" className="inline-block align-middle max-w-[100%] "/>
+                                    <a href="/" className="text-[16px] leading-[22px] font-[500] text-[#000000] w-[57%] ml-[20px] focus:outline-none">Development</a>
+
+
+                                </div>
+                                <div className="w-[32.8%] pl-[20px] border-r-[1px] border-solid border-[#e0e0e0]">
+                                    <img  src="/Home/graphic_design_icon.png" alt="development-icon" className="inline-block align-middle max-w-[100%] "/>
+                                    <a href="/" className="text-[16px] leading-[22px] font-[500] text-[#000000] w-[57%] ml-[20px] focus:outline-none">Designing</a>
+
+
+                                </div>
+                                <div className="w-[32.8%] pl-[20px]  border-solid border-[#e0e0e0] pr-0 box-border">
+                                    <img  src="/Home/digital_marketing_icon.png" alt="development-icon" className="inline-block align-middle max-w-[100%] "/>
+                                    <a href="/" className="text-[16px] leading-[22px] font-[500] text-[#000000] w-[57%] ml-[20px] whitespace-nowrap focus:outline-none">Digital Marketi</a>
+
+
+                                </div>
+
+                            </div>
+
+
+
+
                     </div>
 
 
@@ -58,7 +103,11 @@ function Banner() {
 
 
                     {/* second div */}
-                    <div>
+                    <div className="banner-images w-[39%] mt-[-52px] max-h-[687px] relative float-right box-border">
+                        <figure className="relative">
+                            <img src="/Home/Best_IT_Training_Indore_Student.png" alt="best software training institute in indore "/>
+                        </figure>
+
 
                     </div>
 
