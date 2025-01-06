81e9978d953225ac538abe8c03d02fb445f0a425
rahulsinghchouha <singhrahul15737@gmail.com>
2025-01-06 19:36:50 +0530

    category details banner created

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Testimonial from './Component/Testimonial';
 import Contact from './Component/Contact';
 import CourseDetails from './Component/CourseDetails';
 import BlogDetails from './Component/BlogDetails';
+import CategoryDetails from './Component/Common/CategoryDetailsPage';
 
 
 
@@ -30,6 +31,7 @@ function App() {
           <Route path='/contact-us' element={<Contact/>}/> 
           <Route path='/course-details/:course' element={<CourseDetails/>} key={Math.random()}/>
           <Route path='/blog-details/:blog' element={<BlogDetails/>} key={Math.random()}/>
+          <Route path='/categories-details' element={<CategoryDetails/>} key={Math.random()}/>
         </Routes>
       </Router>
    </div>
--- src/Component/Common/CategoryDetailsPage.jsx
+++ src/Component/Common/CategoryDetailsPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 
 
@@ -6,7 +7,23 @@ const CategoryDetails = () =>{
 
     return(
         <div>
-            
+            <Navbar/>
+            {/* Categories Banner Page Start */}
+            <section className="m-0 pt-[125px]">
+                <div className="relative">
+                    <figure className=" z-[-1]  relative mainImageAfter">
+                        <img className="w-[100%] " src="/Software_Development_Course__Coaching_Indore.jpg" />
+                    </figure>
+                    <div className="wrapper">
+                        <div className="absolute top-[50%] transform translate-y-[-50%] ">
+                            <h1 className="text-[54px] leading-[60px] font-extrabold text-[#ffffff] tracking-[1.62px] ">Software Development</h1>
+                        </div>
+
+                    </div>                    
+                </div>
+            </section>
+            {/* Category Banner Page End */}
+
         </div>
     )
 
