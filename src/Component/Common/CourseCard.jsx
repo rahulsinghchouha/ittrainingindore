f453a7b3146a31dbf14eaf9fb232cd1bcb92fd67
rahulsinghchouha <singhrahul15737@gmail.com>
2025-01-17 16:56:36 +0530

    course small details add succesfully

--- src/Component/Common/CourseCard.jsx
+++ src/Component/Common/CourseCard.jsx
@@ -5,26 +5,34 @@ import { ittrainingDataSerivice } from "../../Services/dataService";
 
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import ConvertAnchorToLink from "./ConvertAnchorToLink";
+import ReactDOMServer from 'react-dom/server';
+import parse from 'html-react-parser';
 
 
 
 
 
-
-
-const CourseCard = ({ cardLimit, square, horizontal,webCard }) => {
+const CourseCard = ({ cardLimit, square, horizontal, webCard }) => {
 
     const navigate = useNavigate();
 
-    function handleCourseDetails (course)
-    {
-        console.log("card",course);
-        navigate("/course-details/" + course.courseName,{state :course });
+    function handleCourseDetails(course) {
+        console.log("card", course);
+        navigate("/course-details/" + course.courseName, { state: course });
     }
 
     const value = ConvertAnchorToLink(String(webCard[0]?.overview));
 
-    console.log("Value",value.slice(0,5));
+    console.log("Value", value.slice(0, 5));
+
+    //Function to safely slice HTML Content
+    const stripHtmlTags = (htmlContent) => {
+        const doc = new DOMParser().parseFromString(htmlContent, 'text/html');
+        return doc.body.textContent || "";
+    };
+
+
+
 
     return (
         <div className="flex flex-wrap justify-center items-center">
@@ -39,7 +47,7 @@ const CourseCard = ({ cardLimit, square, horizontal,webCard }) => {
                         <img src={`${ittrainingDataSerivice.backendUrl}/${card.img}`} alt="Best Web API Development Training Course indore" className="h-[100%] w-[100%] object-contain
                                             rounded-tl-[18px] rounded-tr-[18px]  block "/>
                         <figcaption className="absolute top-[12%] left-[9%] ">
-                            <a href="/" className={`${card.category === "Web Development" && "bg-[#1AAEF4] hover:bg-[#13b440] transition-all delay-0 duration-0 ease-out" || card.category === "Digital Marketing" && "bg-[#4800e2]" ||card.category === "Web Designing" && "bg-[#e3875c]" || card.category === "Graphic Designing" && "bg-[#e8b400]" } pt-[8px] outline-none pr-[16px] pb-[9px] pl-[16px] text-[14px] leading-[19px] font-[700] text-[#ffffff] rounded-[5px] webdevbSha transition-all ease delay-[0.3s] `}>
+                            <a href="/" className={`${card.category === "Web Development" && "bg-[#1AAEF4] hover:bg-[#13b440] transition-all delay-0 duration-0 ease-out" || card.category === "Digital Marketing" && "bg-[#4800e2]" || card.category === "Web Designing" && "bg-[#e3875c]" || card.category === "Graphic Designing" && "bg-[#e8b400]"} pt-[8px] outline-none pr-[16px] pb-[9px] pl-[16px] text-[14px] leading-[19px] font-[700] text-[#ffffff] rounded-[5px] webdevbSha transition-all ease delay-[0.3s] `}>
                                 {card.category}
                             </a>
 
@@ -48,7 +56,7 @@ const CourseCard = ({ cardLimit, square, horizontal,webCard }) => {
                     <div className="pt-[30px] pb-[30px] pl-[28px] pr-[28px] text-left">
                         <div className="mb-[15px] ">
                             <h4 className="text-[18px] leading-[23px] text-[#000000] font-[700] hover:text-[#1AAEF4] transition-all delay-75 ease-linear ">
-                                <button onClick={()=>handleCourseDetails(card)}>
+                                <button onClick={() => handleCourseDetails(card)}>
                                     {card.courseName}
                                 </button>
 
@@ -56,16 +64,12 @@ const CourseCard = ({ cardLimit, square, horizontal,webCard }) => {
                         </div>
                         <div className="mt-[15px] mb-[15px] ml-0 mr-0 min-h-[85px] ">
                             <p className="leading-[26px] text-[16px] text-[#000] font-[400] tracking-normal">
-                                {
-                                 ConvertAnchorToLink(String(card?.overview)).slice(0,5)
-                                }
+                            { stripHtmlTags(card.overview).slice(0, 100) }....
                             </p>
                         </div>
                         <div className="mt-[21px] ">
-                            <button onClick={()=>handleCourseDetails(card)} className=" transform  group-hover:translate-x-3 duration-200  itCardBtn text-[#000000] hover:text-[#1AAEF4] pr-[45px] text-[18px] leading-[23px] font-[700] inline-block transition-all ease delay-75 outline-none ">Details</button>
-
+                            <button onClick={() => handleCourseDetails(card)} className=" transform  group-hover:translate-x-3 duration-200  itCardBtn text-[#000000] hover:text-[#1AAEF4] pr-[45px] text-[18px] leading-[23px] font-[700] inline-block transition-all ease delay-75 outline-none ">Details</button>
                         </div>
-
                     </div>
 
                 </div>
@@ -79,7 +83,7 @@ const CourseCard = ({ cardLimit, square, horizontal,webCard }) => {
                             <img src={`${ittrainingDataSerivice.backendUrl}/${card.img}`} alt="Best Web API Development Training Course indore" className="h-[100%] w-[100%] rounded-[20px] object-cover" />
                             <figcaption className={`absolute top-[12%] left-[4%]`}>
 
-                                <NavLink to="/course" className={`${card.category === "Web Development" && "bg-[#1AAEF4]" || card.category === "Digital Marketing" && "bg-[#4800e2]" ||card.category === "Web Designing" && "bg-[#e3875c]" || card.category === "Graphic Designing" && "bg-[#e8b400]" } pt-[8px] outline-none pr-[16px] pb-[9px] pl-[16px] text-[14px] leading-[19px] font-[700] text-[#ffffff] rounded-[5px] webdevbSha transition-all ease delay-[0.3s] `} >
+                                <NavLink to="/course" className={`${card.category === "Web Development" && "bg-[#1AAEF4]" || card.category === "Digital Marketing" && "bg-[#4800e2]" || card.category === "Web Designing" && "bg-[#e3875c]" || card.category === "Graphic Designing" && "bg-[#e8b400]"} pt-[8px] outline-none pr-[16px] pb-[9px] pl-[16px] text-[14px] leading-[19px] font-[700] text-[#ffffff] rounded-[5px] webdevbSha transition-all ease delay-[0.3s] `} >
                                     {card.category}
                                 </NavLink>
 
@@ -88,7 +92,7 @@ const CourseCard = ({ cardLimit, square, horizontal,webCard }) => {
                         <div className=" w-[69%] pt-[30px] pb-[30px] pl-[28px] pr-[28px] text-left">
                             <div className="mb-[15px] ">
                                 <h4 className="text-[18px] leading-[23px] text-[#000000] font-[700] hover:text-[#1AAEF4] transition-all delay-75 ease-linear ">
-                                    <button onClick={()=>handleCourseDetails(card)}>
+                                    <button onClick={() => handleCourseDetails(card)}>
                                         {card.courseName}
                                     </button>
 
@@ -96,12 +100,12 @@ const CourseCard = ({ cardLimit, square, horizontal,webCard }) => {
                             </div>
                             <div className="mt-[15px] mb-[15px] ml-0 mr-0 min-h-[85px] ">
                                 <p className="leading-[26px] text-[16px] text-[#000] font-[400] tracking-normal">
-                                {card.overview.slice(0,120)} .....
+                                    {card.overview.slice(0, 120)} .....
                                 </p>
 
                             </div>
                             <div className="mt-[21px] ">
-                                <button onClick={()=>handleCourseDetails(card)} className=" transform  group-hover:translate-x-3 duration-200  itCardBtn text-[#000000] hover:text-[#1AAEF4] pr-[45px] text-[18px] leading-[23px] font-[700] inline-block transition-all ease delay-75 outline-none ">Details</button>
+                                <button onClick={() => handleCourseDetails(card)} className=" transform  group-hover:translate-x-3 duration-200  itCardBtn text-[#000000] hover:text-[#1AAEF4] pr-[45px] text-[18px] leading-[23px] font-[700] inline-block transition-all ease delay-75 outline-none ">Details</button>
 
                             </div>
 
